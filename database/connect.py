d3f2fcba50609d9843ec073f0a901043f4e04839
Saulo Felipe <saulo234567@gmail.com>
2022-11-20 15:58:38 -0300

    finish migrating

--- database/connect.py
+++ database/connect.py
@@ -1,10 +1,10 @@
 import psycopg2
 
 conn = psycopg2.connect(
-    host="db.iiyunbxnpsorezcvitbs.supabase.co",
-    database="postgres",
-    user="postgres",
-    password="the-best-hero")
+    host="babar.db.elephantsql.com",
+    database="gdbhfzwz",
+    user="gdbhfzwz",
+    password="Lb4-wkTyUVoTCnES3syU2TF49x9Cg2cJ")
 
 conn.set_session(autocommit=True)
 
